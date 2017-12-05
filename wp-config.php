ab1b7ae5ecef7a485c9f2b0d338e2317912fd429
yudwcom <yudwcom@163.com>
2017-12-05 14:14:23 +0800

    设置可以安装模板

--- wp-config.php
+++ wp-config.php
@@ -94,3 +94,7 @@ if ( !defined('ABSPATH') )
 
 /** 设置WordPress变量和包含文件。 */
 require_once(ABSPATH . 'wp-settings.php');
+
+define("FS_METHOD", "direct");
+define("FS_CHMOD_DIR", 0777);
+define("FS_CHMOD_FILE", 0777);
